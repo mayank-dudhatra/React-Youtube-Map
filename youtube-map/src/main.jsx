240c8a9cda1a764757f638458cd022f403f22bdc
Mayank Dudhatra <mayankdudhatracg@gmail.com>
2024-12-05 18:47:50 +0530

    Components and Inside Components We created a file . And Conncet or Import The all Files.

--- youtube-map/src/main.jsx
+++ youtube-map/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Sidebar from './components/searchbar/searchbar.jsx'
+import Maincontent from './components/maincontent/maincontent.jsx'
+import Searchbar from './components/searchbar/searchbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <div className="part-1">
+    <Sidebar />
+    <div className='part-2'>
+    <Searchbar />
+    <Maincontent />
+    </div>
+    </div>
   </StrictMode>,
 )
